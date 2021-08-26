40e5a28ce66634e17fa211b77da9e0cfbb14eae4
ElectricGoal <minhtu250802@gmail.com>
2021-08-26 22:27:52 +0700

    update textField

--- src/lib/screens/search_screen.dart
+++ src/lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:keyboard_dismisser/keyboard_dismisser.dart';
 import 'package:weather_app/models/location_search.dart';
 import 'package:weather_app/provider/navigation_provider.dart';
 import 'package:weather_app/services/search_service.dart';
@@ -23,7 +24,7 @@ class _SearchScreenState extends State<SearchScreen> {
   late Stream _stream;
 
   Timer? _debouncer;
-  
+
   ///Search location method
   void _search() async {
     if (_textController.text == null || _textController.text.length == 0) {
@@ -47,6 +48,7 @@ class _SearchScreenState extends State<SearchScreen> {
   void initState() {
     _streamController = StreamController();
     _stream = _streamController.stream;
+    _textController.addListener(() => setState(() {}));
     super.initState();
   }
 
@@ -70,52 +72,57 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final FocusScopeNode currentScope = FocusScope.of(context);
-        if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-          FocusManager.instance.primaryFocus!.unfocus();
-        }
-      },
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        drawer: MenuDrawer(),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Add location'),
+    return KeyboardDismisser(
+      gestures: [
+        GestureType.onTap,
+      ],
+      child: GestureDetector(
+        // onTap: () {
+        //   final FocusScopeNode currentScope = FocusScope.of(context);
+        //   if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
+        //     FocusManager.instance.primaryFocus!.unfocus();
+        //   }
+        // },
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(60),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
+          drawer: MenuDrawer(),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Add location'),
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(60),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: buildTextField(),
               ),
-              child: buildTextField(),
             ),
           ),
-        ),
-        body: Container(
-          decoration: backgroundGradient,
-          child: StreamBuilder(
-            stream: _stream,
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasError) {
-                return _ErrorCase();
-              }
-              if (snapshot.data == 'No data') {
-                return _NotFoundCase();
-              }
+          body: Container(
+            decoration: backgroundGradient,
+            child: StreamBuilder(
+              stream: _stream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasError) {
+                  return _ErrorCase();
+                }
+                if (snapshot.data == 'No data') {
+                  return _NotFoundCase();
+                }
 
-              switch (snapshot.data) {
-                case null:
-                  return _NullCase();
-                case 'waiting':
-                  return _WaitingCase();
-                default:
-                  return _buildLocationList(snapshot);
-              }
-            },
+                switch (snapshot.data) {
+                  case null:
+                    return _NullCase();
+                  case 'waiting':
+                    return _WaitingCase();
+                  default:
+                    return _buildLocationList(snapshot);
+                }
+              },
+            ),
           ),
         ),
       ),
@@ -139,13 +146,15 @@ class _SearchScreenState extends State<SearchScreen> {
       controller: _textController,
       cursorColor: Colors.green,
       decoration: InputDecoration(
-        suffixIcon: IconButton(
-          icon: Icon(
-            Icons.close,
-            color: Colors.grey,
-          ),
-          onPressed: _textController.clear,
-        ),
+        suffixIcon: _textController.text.isEmpty
+            ? Container(width: 0)
+            : IconButton(
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+                onPressed: _textController.clear,
+              ),
         contentPadding: EdgeInsets.only(left: 30),
         filled: true,
         fillColor: Colors.white,
@@ -168,49 +177,49 @@ class _SearchScreenState extends State<SearchScreen> {
 }
 
 Widget _buildLocationList(AsyncSnapshot snapshot) {
-    final locationList = snapshot.data;
-    return Consumer<NavigationProvider>(
-      builder: (context, navigation, __) => ListView.builder(
-        itemCount: locationList.length,
-        itemBuilder: (context, index) {
-          return Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 10,
-                    left: 10,
-                    right: 10,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () {
-                          navigation.addLocation(
-                            locationList[index].name,
-                            locationList[index].id,
-                          );
-                          navigation.setSelectedIdx(
-                            navigation.menuItems.length - 1,
-                          );
-                        },
-                        child: LocationDetail(
-                          name: locationList[index].name,
-                          type: locationList[index].type,
-                        ),
+  final locationList = snapshot.data;
+  return Consumer<NavigationProvider>(
+    builder: (context, navigation, __) => ListView.builder(
+      itemCount: locationList.length,
+      itemBuilder: (context, index) {
+        return Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10,
+                  left: 10,
+                  right: 10,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {
+                        navigation.addLocation(
+                          locationList[index].name,
+                          locationList[index].id,
+                        );
+                        navigation.setSelectedIdx(
+                          navigation.menuItems.length - 1,
+                        );
+                      },
+                      child: LocationDetail(
+                        name: locationList[index].name,
+                        type: locationList[index].type,
                       ),
                     ),
                   ),
                 ),
               ),
-            ],
-          );
-        },
-      ),
-    );
-  }
+            ),
+          ],
+        );
+      },
+    ),
+  );
+}
 
 class _WaitingCase extends StatelessWidget {
   const _WaitingCase({
