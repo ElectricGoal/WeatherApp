7ea950d2aac1e4f022bd503c3a85cbb850105b4e
ElectricGoal <minhtu250802@gmail.com>
2021-09-02 21:09:45 +0700

    modify import path

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/provider/navigation_provider.dart';
-import 'package:weather_app/size_config.dart';
+import 'package:weather_app/responsive/size_config.dart';
 
 void main() {
   runApp(MyApp());
